f6d3cf9ae18693472b6f042e025cf164cf33a798
Ilya malgin <soundmaker1505@gmail.com>
2021-01-08 15:10:54 +0300

    Keybindings manager class added

---
+++ src/keybindings_manager.cpp
@@ -0,0 +1,27 @@
+#include "KeybindingsManager"
+
+namespace Ouquitoure
+{
+
+    KeybindingsManager::KeybindingsManager()
+        : cameraKeys( CAMERA_MOVE_DIRECTION::NUM_DIRECTIONS )
+    {
+        cameraKeys[ FORWARD ]  = Qt::Key_W;
+        cameraKeys[ BACKWARD ] = Qt::Key_S;
+        cameraKeys[ RIGHT ]    = Qt::Key_D;
+        cameraKeys[ LEFT ]     = Qt::Key_A;
+        cameraKeys[ UP ]       = Qt::Key_E;
+        cameraKeys[ DOWN ]     = Qt::Key_Shift;
+    }
+
+    Qt::Key KeybindingsManager::getCameraControlsKey( CAMERA_MOVE_DIRECTION moveDirection )
+    {
+        return cameraKeys[ moveDirection ];
+    }
+
+    void KeybindingsManager::setCameraControlsKey( CAMERA_MOVE_DIRECTION moveDirection, Qt::Key newKey )
+    {
+        cameraKeys[ moveDirection ] = newKey;
+    }
+
+} // namespace Ouquitoure
---
+++ src/keybindings_manager.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <CameraMoveDirection>
+
+#include <QObject>
+#include <parallel_hashmap/phmap.h>
+
+namespace Ouquitoure
+{
+    /**
+     * @brief The manager of all the key bindings of the Ouquitoure
+     */
+    class KeybindingsManager : public QObject
+    {
+        Q_OBJECT
+    public:
+        KeybindingsManager();
+
+        Qt::Key getCameraControlsKey( CAMERA_MOVE_DIRECTION moveDirection );
+        void    setCameraControlsKey( CAMERA_MOVE_DIRECTION moveDirection, Qt::Key newKey );
+
+    signals:
+        void cameraControlsChanged();
+
+    private:
+        phmap::flat_hash_map<CAMERA_MOVE_DIRECTION, Qt::Key> cameraKeys;
+    };
+} // namespace Ouquitoure
